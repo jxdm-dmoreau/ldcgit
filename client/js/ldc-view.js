52d4cd9c1f7d9a1b138258d559af16998cff3309
David Moreau <dmoreau@asrock.(none)>
2010-02-28 20:32:28 +0100

    Big code refactor

--- client/js/ldc-view.js
+++ client/js/ldc-view.js
@@ -1,220 +1,226 @@
-function register_listener () {
-}
-function ldc_view_init () {
+/******************************************************************************
+  * VIEW
+******************************************************************************/
+ldc.view = function () {
     $("#main").hide();
     $("#log").empty().append('<div id="pb"></div>');
     $("#pb").css('width', '300px');
     $("#pb").progressbar({ value: 1 });
-}
+};
 
-function ldc_load_completed() {
-    ldc_view_init_operations();
-    ldc_view_init_categories();
-    $("#log").empty();
-    $("#main").show();
-}
 
-function ldc_load(percent) {
+
+
+/******************************************************************************
+  * LOAD
+******************************************************************************/
+
+ldc.view.load = function (percent) {
     var length = percent * $("#pb").width();
     $("#pb").progressbar('option', 'value', length);
-}
+};
+
+ldc.view.load.completed = function () {
+    ldc.view.operations.init();
+    ldc.view.categories.init();
+    $("#log").empty();
+    $("#main").show();
+};
 
 /******************************************************************************
   * LOG
 ******************************************************************************/
-function ldc_view_log_error(text) {
-    $("#log").empty().addClass('error').append(text);
+ldc.view.log = function(msg) {
+    console.debug(msg);
+};
+ldc.log = ldc.view.log;
+
+ldc.view.log.success = function (msg) {
+    $("#log").empty().addClass('success').append(msg);
 }
 
-function ldc_view_log_success(text) {
-    $("#log").empty().addClass('success').append(text);
+ldc.view.log.error = function (msg) {
+    $("#log").empty().addClass('error').append(msg);
 }
+
+/******************************************************************************
+  * view operations
+******************************************************************************/
+
 
 
 /******************************************************************************
- * OPERATIONS
+ * init
 ******************************************************************************/
-function ldc_view_init_operations() {
-
-    /* Display operations */
-    function view_init() {
-
-        function operation2html(op, compte_id) {
-            var html = '<tr>';
-            html += '<td>'+op.id+'</td>';
-            html += '<td>'+op.date+'</td>';
-            if (compte_id == op.from) {
-                html += '<td>';
-                html += '<ul>';
-                for(i in op.cats) {
-                    html += '<li>'+op.cats[i].val+'</li>';
-                }
-                html += '</ul>';
-                html += '</td>'
-                html += '<td>0</td>';
-            } else {
-                html += '<td>0</td>';
-                html += '<td><ul>';
-                for(i in op.cats) {
-                    html += '<li>'+op.cats[i].val+'</li>';
-                }
-                html += '</ul></td>';
-            }
-            html += '<td><ul>';
-            for(i in op.cats) {
-                html += '<li>'+ldc_cat_get_name(op.cats[i].cat_id)+'</li>';
-            }
-            html += '</ul></td>';
-            html += '<td>'+op.description+'</td>'
-            html += '</tr>';
-            return html;
+ldc.view.operations = function (op, compte_id) {
+    var html = '<tr>';
+    html += '<td>'+op.id+'</td>';
+    html += '<td>'+op.date+'</td>';
+    if (compte_id == op.from) {
+        html += '<td>';
+        html += '<ul>';
+        for(i in op.cats) {
+            html += '<li>'+op.cats[i].val+'</li>';
         }
-
-        for(var i in LDC_COMPTES) {
-            var jDiv = $('<div>').append('<h1>'+LDC_COMPTES[i].bank+' - '+LDC_COMPTES[i].name+'</h1>');
-            var jTable = $('<table>');
-            var table_head = '<thead><th>id</th><th>date</th><th>Débit</th><th>Crédit</th><th>Catégories</th><th>Description</th></thead>';
-            var jTable = $('<table>').append(table_head);
-            var id = LDC_COMPTES[i].id;
-            for (var j in LDC_OPERATIONS) {
-                var from = LDC_OPERATIONS[j].from;
-                var to =  LDC_OPERATIONS[j].to;
-                if (from == id | to == id) {
-                    jTable.append(operation2html(LDC_OPERATIONS[j], i));
-                }
-            }
-            jDiv.append(jTable);
-            $("#operations").append(jDiv);
+        html += '</ul>';
+        html += '</td>'
+        html += '<td>0</td>';
+    } else {
+        html += '<td>0</td>';
+        html += '<td><ul>';
+        for(i in op.cats) {
+            html += '<li>'+op.cats[i].val+'</li>';
         }
+        html += '</ul></td>';
+    }
+    html += '<td><ul>';
+    for(i in op.cats) {
+        html += '<li>'+ldc_cat_get_name(op.cats[i].cat_id)+'</li>';
+    }
+    html += '</ul></td>';
+    html += '<td>'+op.description+'</td>'
+    html += '</tr>';
+    return html;
+};
+
+ldc.view.operations.init = function() {
+
+    /* VIEW */
+    for(var i in ldc.COMPTES) {
+        var jDiv = $('<div>').append('<h1>'+ldc.COMPTES[i].bank+' - '+ldc.COMPTES[i].name+'</h1>');
+        var jTable = $('<table compte_id="'+ldc.COMPTES[i].id+'">');
+        var table_head = '<thead><th>id</th><th>date</th><th>Débit</th><th>Crédit</th><th>Catégories</th><th>Description</th></thead>';
+        jTable.append(table_head);
+        var id = ldc.COMPTES[i].id;
+        for (var j in ldc.OPERATIONS) {
+            var from = ldc.OPERATIONS[j].from;
+            var to =  ldc.OPERATIONS[j].to;
+            if (from == id | to == id) {
+                jTable.append(ldc.view.operations(ldc.OPERATIONS[j], i));
+            }
+        }
+        jDiv.append(jTable);
+        $("#operations").append(jDiv);
     }
 
-  /* Actions on operations */ 
-    function view_init_actions() {
-        $("div#operations button.add").click(function() {
-                var op = { from:1, to:1, date:'2010-01-01', confirm:1, cats: { id:1, value:12}};
-                ldc_op_add(op);
-        });
-        $("div#operations button.update").click(function() {
-                var op = { id:46, from:2, to:3, date:'2010-01-01', confirm:1, cats: { id:1, value:12}};
-                ldc_op_update(op);
-        });
-        $("div#operations button.del").click(function() {
-                ldc_op_del(45);
-        });
+    /* ACTIONS */
+    /* function to add operation in the HTML table */
+    function view_add_operation(op) {
+        $("table[compte_id="+op.from+"]").css('color', 'red');
     }
 
-    /* main */
-    view_init();
-    view_init_actions();
+    $("div#operations button.add").click(function() {
+            var op = { from:3, to:1, date:'2010-01-01', confirm:1, cats: { id:1, value:12}};
+            //view_add_operation(op);
+            ldc.operations.add(op);
+    });
+    $("div#operations button.update").click(function() {
+            var op = { id:46, from:2, to:3, date:'2010-01-01', confirm:1, cats: { id:1, value:12}};
+            ldc.operations.update(op);
+    });
+    $("div#operations button.del").click(function() {
+            ldc.operations.del(45);
+    });
+};
 
-}
 
 
 /******************************************************************************
   * CATEGORIES
 ******************************************************************************/
-function ldc_view_init_categories() {
+ldc.view.categories = {};
+ldc.view.categories.init = function () {
 
-    /* init view */
-    function view_init() {
-
-        function display_cat_r(cat, html) {
-            html += '<li cat_id="'+cat.id+'"><a href="#"><ins>&nbsp;</ins>'+cat.name+'</a>';
-            var children = ldc_cat_get_children(cat.id);
-            if (children.length > 0) {
-                html += '<ul>';
-                for(var i in children) {
-                    html = display_cat_r(children[i], html);
-                }
-                html += '</ul>';
+    /* VIEW */
+    function display_cat_r(cat, html) {
+        html += '<li cat_id="'+cat.id+'"><a href="#"><ins>&nbsp;</ins>'+cat.name+'</a>';
+        var children = ldc_cat_get_children(cat.id);
+        if (children.length > 0) {
+            html += '<ul>';
+            for(var i in children) {
+                html = display_cat_r(children[i], html);
             }
-            html += '</li>';
-            return html;
+            html += '</ul>';
         }
-
-        function onrename_categories(node) {
-            var name = $(node).children("a").text().substr(1);
-            var father_id = $(node).parent("ul").parent("li").attr("cat_id");
-            if (is_creation) {
-                var id = ldc_cat_add(name, father_id);
-                $(node).attr("cat_id",id);
-                is_creation = false;
-
-            } else if (is_update) {
-                var id =  $(node).attr("cat_id");
-                console.debug("ldc_cat_update("+id+","+name+","+father_id+")");
-                ldc_cat_update(id, name, father_id);
-                id_update = false;
-            }
-        }
-        function ondelete_categories(node) {
-            var id =  $(node).attr("cat_id");
-            ldc_cat_del(id);
-        }
-        function onmove_categories(NODE,REF_NODE,TYPE,TREE_OBJ,RB) {
-            var name = $(NODE).children("a").text().substr(1);
-            var father_id = $(NODE).parent("ul").parent("li").attr("cat_id");
-            var id =  $(NODE).attr("cat_id");
-            ldc_cat_update(id, name, father_id);
-        }
-
-        html = '<ul>';
-        html += '<li rel="root" cat_id="0"><a href="#"><ins>&nbsp;</ins>Catégories</a><ul>';
-        var children = ldc_cat_get_children(0);
-        for(var i in children) {
-            html = display_cat_r(children[i], html);
-        }
-        html += '</ul></li></ul>';
-        $("#cats").append(html);
-
-        ;
-        $("#cats").tree( {
-            callback: {
-                onrename : onrename_categories,
-                ondelete : ondelete_categories,
-                onmove   : onmove_categories
-            },
-            types: {
-                "root" : {
-                    clickable   : true,
-                    deletable   : false,
-                    draggable   : false,
-                }
-            }
-
-        });
+        html += '</li>';
+        return html;
     }
 
-    function view_init_actions() {
-        /* buttons */
-        $("div#cats_menu button.add").click(function() {
-                is_creation = true;
-                var t = $.tree.focused();
-                if(t.selected) {
-                    t.create();
-                } else {
-                    alert("Select a node first");
-                }
-            }
-        );
-        $("div#cats_menu button.del").click(function() {
-            if(confirm("Voulez-vous vraiment supprimer cette catégorie ?")) {
-                    $.tree.focused().remove();
-                    }
-            })
-        ;
-        $("div#cats_menu button.rename").click(function() {
-                    is_update = true;
-                    $.tree.focused().rename();
-            })
+    html = '<ul>';
+    html += '<li rel="root" cat_id="0"><a href="#"><ins>&nbsp;</ins>Catégories</a><ul>';
+    var children = ldc.categories.get_children(0);
+    for(var i in children) {
+        html = display_cat_r(children[i], html);
     }
+    html += '</ul></li></ul>';
+    $("#cats").append(html);
 
-    /* main */
+    $("#cats").tree( {
+        callback: {
+            onrename : onrename_categories,
+            ondelete : ondelete_categories,
+            onmove   : onmove_categories
+        },
+        types: {
+            "root" : {
+                clickable   : true,
+                deletable   : false,
+                draggable   : false,
+            }
+        }
+    });
+
+    /* ACTIONS */
     var is_creation = false;
     var is_update = false;
-    view_init();
-    view_init_actions();
 
+    function onrename_categories(node) {
+        var name = $(node).children("a").text().substr(1);
+        var father_id = $(node).parent("ul").parent("li").attr("cat_id");
+        if (is_creation) {
+            var id = ldc.categories.add(name, father_id);
+            $(node).attr("cat_id",id);
+            is_creation = false;
 
+        } else if (is_update) {
+            var id =  $(node).attr("cat_id");
+            console.debug("ldc_cat_update("+id+","+name+","+father_id+")");
+            ldc.categories.update(id, name, father_id);
+            id_update = false;
+        }
+    }
 
-}
+    function ondelete_categories(node) {
+        var id =  $(node).attr("cat_id");
+        ldc.categories.del(id);
+    }
+
+    function onmove_categories(NODE,REF_NODE,TYPE,TREE_OBJ,RB) {
+        var name = $(NODE).children("a").text().substr(1);
+        var father_id = $(NODE).parent("ul").parent("li").attr("cat_id");
+        var id =  $(NODE).attr("cat_id");
+        ldc.categories.update(id, name, father_id);
+    }
+
+    $("div#cats_menu button.add").click(function() {
+        is_creation = true;
+        var t = $.tree.focused();
+        if(t.selected) {
+            t.create();
+        } else {
+            alert("Select a node first");
+        }
+    });
+
+    $("div#cats_menu button.del").click(function() {
+        if(confirm("Voulez-vous vraiment supprimer cette catégorie ?")) {
+            $.tree.focused().remove();
+        }
+    });
+
+    $("div#cats_menu button.rename").click(function() {
+                is_update = true;
+                $.tree.focused().rename();
+    });
+
+};
--- client/js/ldc.js
+++ client/js/ldc.js
@@ -1,59 +1,211 @@
-/* MES VARIABLES */
-var LDC_SERVER="http://192.168.1.6/ldc/server/"
-var LDC_CATEGORIES;
-var LDC_COMPTES;
-var LDC_OPERATIONS;
-var LDC_NB_LOAD = 0;
-var LDC_TOTAL_LOAD = 3;
 
-/* FUNCTIONS */
-function ldc_post_ajax(url, data, f_success, async)
-{
+
+var ldc = {};
+ldc.SERVER="http://192.168.1.6/ldc/server/";
+ldc.CATEGORIES = [];
+ldc.COMPTES = [];
+ldc.OPERATIONS = [];
+ldc.nb_load = 0;
+ldc.TOTAL_LOAD = 3;
+
+/******************************************************************************
+*  various functions
+******************************************************************************/
+ldc.post_ajax = function (url, data, f_success, async) {
     function f_error(XMLHttpRequest, textStatus, errorThrown) {
-        ldc_view_log_error(textStatus+' '+errorThrown);
+        ldc.view.log.error(textStatus+' '+errorThrown);
     }
     if (async == undefined) {
         async = true;
     }
     var param = { type: "POST", url: url, success: f_success, async: async, data: data, error: f_error};
     $.ajax(param);
-}
+};
+
+
+
+ldc.load = function () {
+    ldc.nb_load++;
+    ldc.view.load(ldc.nb_load/ldc.TOTAL_LOAD);
+    if (ldc.nb_load == ldc.TOTAL_LOAD) {
+        ldc.view.load.completed();
+    }
+};
+
+ldc.register = function () {
+    ldc.view();
+    $.getJSON(ldc.SERVER + "get_categories.php", ldc.categories.store);
+    $.getJSON(ldc.SERVER + "get_comptes.php",   ldc.comptes.store);
+    var data = { date_begin:'2000-01-01', date_end:'2020-12-12'};
+    jQuery.post(ldc.SERVER + "get_operation.php",  "json="+JSON.stringify(data) , ldc.operations.store)
+};
+
+/******************************************************************************
+*  Categories functions
+******************************************************************************/
+ldc.categories = {};
+
+ldc.categories.store = function (data, textStatus) {
+    ldc.CATEGORIES = data;
+    ldc.load();
+};
+
+
+ldc.categories.get_children = function (id) { 
+    var children = [];
+    for(var i in ldc.CATEGORIES) {
+        if (ldc.CATEGORIES[i].father_id == id) {
+            children.push(ldc.CATEGORIES[i]);
+        }
+    }
+    return children;
+};
+
+ldc.categories.get_name= function (id) {
+    for (i in ldc.CATEGORIES) {
+        if (ldc.CATEGORIES[i].id == id) {
+            return ldc.CATEGORIES[i].name;
+        }
+    }
+    return "unknown";
+};
+
+ldc.categories.add = function (name, father_id) {
+    var id;
+    function on_success(data, textStatus) {
+        if (data == 1) {
+            ldc.view.log.error("L'ajout de la catégorie a échoué...");
+        } else {
+            ldc.view.log.success("Catégorie ajoutée.");
+        }
+        data = JSON.parse(data);
+        id = data.id;
+    }
+    var data = { name: name, father_id: father_id };
+    ldc.post_ajax(ldc.SERVER + "add_categorie.php",  "json="+JSON.stringify(data) , on_success, false);
+    data.id = ''+id;
+    ldc.CATEGORIES.push(data);
+    return id;
+};
+
+ldc.categories.del = function (id) {
+    function on_success(data, textStatus) {
+        if (data == 1) {
+            ldc.view.log.error("La suppresion de la catégorie a échoué...");
+        } else {
+            ldc.view.log.success("Catégorie supprimée.");
+        }
+    }
+    var data = { id: id };
+    ldc.post_ajax(ldc.SERVER + "del_categorie.php",  "json="+JSON.stringify(data) , on_success, true);
+    for(var i in ldc.CATEGORIES) {
+        if (ldc.CATEGORIES[i].id == id) {
+            delete(ldc.CATEGORIES[i]);
+            return;
+        }
+    }
+};
+
+ldc.categories.update = function(id, name, father_id) {
+    function on_success(data, textStatus) {
+        if (data == 1) {
+            ldc.view.log.error("La Modification de la catégorie a échoué...");
+        } else {
+            ldc.view.log.success("Catégorie modifiée.");
+        }
+    }
+    var data = { id: id, name: name, father_id: father_id };
+    ldc.post_ajax(ldc.SERVER + "update_categorie.php",  "json="+JSON.stringify(data) , on_success, false);
+    for(var i in ldc.CATEGORIES) {
+        if (ldc.CATEGORIES[i].id == data.id) {
+            ldc.CATEGORIES[i] = data;
+            return;
+        }
+    }
+};
+
+/******************************************************************************
+*  Compte functions
+******************************************************************************/
+ldc.comptes = {};
+
+ldc.comptes.store = function (data, textStatus) {
+    ldc.COMPTES = data;
+    ldc.load();
+};
+
+
+/******************************************************************************
+*  Opérations functions
+******************************************************************************/
+ldc.operations = {};
+
+ldc.operations.store = function (data, textStatus) {
+    ldc.OPERATIONS = JSON.parse(data);
+    ldc.load();
+};
+
+ldc.operations.add= function (op) {
+    function on_success(data, textStatus) {
+        if (data == 1) {
+            ldc.view.log.error("L'ajout de l'opération a échoué...");
+        } else {
+            ldc.view.log.success("Opération ajoutée.");
+        }
+        data = JSON.parse(data);
+        op.id = data.id;
+    }
+    ldc.post_ajax(ldc.SERVER + "add_operation.php",  "json="+JSON.stringify(op) , on_success, false);
+    ldc.OPERATIONS.push(op);
+};
+
+ldc.operations.del = function(id) {
+    function on_success(data, textStatus) {
+        if (data == 1) {
+            ldc.view.log.error("La suppression de l'opération a échoué...");
+        } else {
+            ldc.view.log.success("Opération supprimée.");
+        }
+    }
+    var data = { id: id};
+    ldc.post_ajax(ldc.SERVER + "del_operation.php",  "json="+JSON.stringify(data) , on_success, true);
+    for(var i in ldc.OPERATIONS) {
+        if (ldc.OPERATIONS[i].id == id) {
+            delete(ldc.OPERATIONS[i]);
+            break;
+        }
+    }
+};
+
+ldc.operations.update = function (op) {
+    function on_success(data, textStatus) {
+        if (data == 1) {
+            ldc.view.log.error("La modification de l'opération a échoué...");
+        } else {
+            ldc.view.log.success("Opération Modifiée.");
+        }
+    }
+    ldc.post_ajax(ldc.SERVER + "update_operation.php",  "json="+JSON.stringify(op) , on_success, true);
+    for(var i in ldc.OPERATIONS) {
+        if (ldc.OPERATIONS[i].id == op.id) {
+            ldc.OPERATIONS[i] = op;
+            break;
+        }
+    }
+};
+
+
+
+
+
+
+
+window.ldc = ldc;
 
 
 /******************************************************************************
 *  Init Functions
 ******************************************************************************/
-function ldc_is_load_completed() {
-    LDC_NB_LOAD++;
-    ldc_load(LDC_NB_LOAD/LDC_TOTAL_LOAD);
-    if (LDC_NB_LOAD == LDC_TOTAL_LOAD) {
-        ldc_load_completed();
-    }
-}
-
-function ldc_store_categories(data, textStatus) {
-    LDC_CATEGORIES = data;
-    ldc_is_load_completed();
-}
-
-function ldc_store_comptes(data, textStatus) {
-    LDC_COMPTES = data;
-    ldc_is_load_completed();
-}
-function ldc_store_operations(data, textStatus) {
-    LDC_OPERATIONS = JSON.parse(data);
-    ldc_is_load_completed();
-}
-
-
-function ldc_register()
-{
-    ldc_view_init();
-    $.getJSON(LDC_SERVER + "get_categories.php", ldc_store_categories);
-    $.getJSON(LDC_SERVER + "get_comptes.php",   ldc_store_comptes);
-    var data = { date_begin:'2000-01-01', date_end:'2020-12-12'};
-    jQuery.post(LDC_SERVER + "get_operation.php",  "json="+JSON.stringify(data) , ldc_store_operations)
-}
 
 
 /******************************************************************************
@@ -61,9 +213,9 @@ function ldc_register()
 ******************************************************************************/
 function ldc_cat_get_children(id) { 
     var children = [];
-    for(var i in LDC_CATEGORIES) {
-        if (LDC_CATEGORIES[i].father_id == id) {
-            children.push(LDC_CATEGORIES[i]);
+    for(var i in ldc.CATEGORIES) {
+        if (ldc.CATEGORIES[i].father_id == id) {
+            children.push(ldc.CATEGORIES[i]);
         }
     }
     return children;
@@ -71,9 +223,9 @@ function ldc_cat_get_children(id) {
 
 function ldc_cat_get_name(id)
 {
-    for (i in LDC_CATEGORIES) {
-        if (LDC_CATEGORIES[i].id == id) {
-            return LDC_CATEGORIES[i].name;
+    for (i in ldc.CATEGORIES) {
+        if (ldc.CATEGORIES[i].id == id) {
+            return ldc.CATEGORIES[i].name;
         }
 
     }
@@ -84,33 +236,33 @@ function ldc_cat_add(name, father_id) {
     var id;
     function on_success(data, textStatus) {
         if (data == 1) {
-            ldc_view_log_error("L'ajout de la catégorie a échoué...");
+            ldc.view.log.error("L'ajout de la catégorie a échoué...");
         } else {
-            ldc_view_log_success("Catégorie ajoutée.");
+            ldc.view.log.success("Catégorie ajoutée.");
         }
         data = JSON.parse(data);
         id = data.id;
     }
     var data = { name: name, father_id: father_id };
-    ldc_post_ajax(LDC_SERVER + "add_categorie.php",  "json="+JSON.stringify(data) , on_success, false);
+    ldc.post_ajax(LDC_SERVER + "add_categorie.php",  "json="+JSON.stringify(data) , on_success, false);
     data.id = ''+id;
-    LDC_CATEGORIES.push(data);
+    ldc.CATEGORIES.push(data);
     return id;
 }
 
 function ldc_cat_del(id) {
     function on_success(data, textStatus) {
         if (data == 1) {
-            ldc_view_log_error("La suppresion de la catégorie a échoué...");
+            ldc.view.log.error("La suppresion de la catégorie a échoué...");
         } else {
-            ldc_view_log_success("Catégorie supprimée.");
+            ldc.view.log.success("Catégorie supprimée.");
         }
     }
     var data = { id: id };
-    ldc_post_ajax(LDC_SERVER + "del_categorie.php",  "json="+JSON.stringify(data) , on_success, true);
-    for(var i in LDC_CATEGORIES) {
-        if (LDC_CATEGORIES[i].id == id) {
-            delete(LDC_CATEGORIES[i]);
+    ldc.post_ajax(LDC_SERVER + "del_categorie.php",  "json="+JSON.stringify(data) , on_success, true);
+    for(var i in ldc.CATEGORIES) {
+        if (ldc.CATEGORIES[i].id == id) {
+            delete(ldc.CATEGORIES[i]);
             return;
         }
     }
@@ -119,16 +271,16 @@ function ldc_cat_del(id) {
 function ldc_cat_update(id, name, father_id) {
     function on_success(data, textStatus) {
         if (data == 1) {
-            ldc_view_log_error("La Modification de la catégorie a échoué...");
+            ldc.view.log.error("La Modification de la catégorie a échoué...");
         } else {
-            ldc_view_log_success("Catégorie modifiée.");
+            ldc.view.log.success("Catégorie modifiée.");
         }
     }
     var data = { id: id, name: name, father_id: father_id };
-    ldc_post_ajax(LDC_SERVER + "update_categorie.php",  "json="+JSON.stringify(data) , on_success, false);
-    for(var i in LDC_CATEGORIES) {
-        if (LDC_CATEGORIES[i].id == data.id) {
-            LDC_CATEGORIES[i] = data;
+    ldc.post_ajax(LDC_SERVER + "update_categorie.php",  "json="+JSON.stringify(data) , on_success, false);
+    for(var i in ldc.CATEGORIES) {
+        if (ldc.CATEGORIES[i].id == data.id) {
+            ldc.CATEGORIES[i] = data;
             return;
         }
     }
@@ -149,30 +301,30 @@ function ldc_somme(op)
 function ldc_op_add(op) {
     function on_success(data, textStatus) {
         if (data == 1) {
-            ldc_view_log_error("L'ajout de l'opération a échoué...");
+            ldc.view.log.error("L'ajout de l'opération a échoué...");
         } else {
-            ldc_view_log_success("Opération ajoutée.");
+            ldc.view.log.success("Opération ajoutée.");
         }
         data = JSON.parse(data);
         op.id = data.id;
     }
-    ldc_post_ajax(LDC_SERVER + "add_operation.php",  "json="+JSON.stringify(op) , on_success, false);
-    LDC_OPERATIONS.push(op);
+    ldc.post_ajax(LDC_SERVER + "add_operation.php",  "json="+JSON.stringify(op) , on_success, false);
+    ldc.OPERATIONS.push(op);
 }
 
 function ldc_op_del(id) {
     function on_success(data, textStatus) {
         if (data == 1) {
-            ldc_view_log_error("La suppression de l'opération a échoué...");
+            ldc.view.log.error("La suppression de l'opération a échoué...");
         } else {
-            ldc_view_log_success("Opération supprimée.");
+            ldc.view.log.success("Opération supprimée.");
         }
     }
     var data = { id: id};
-    ldc_post_ajax(LDC_SERVER + "del_operation.php",  "json="+JSON.stringify(data) , on_success, true);
-    for(var i in LDC_OPERATIONS) {
-        if (LDC_OPERATIONS[i].id == id) {
-            delete(LDC_OPERATIONS[i]);
+    ldc.post_ajax(LDC_SERVER + "del_operation.php",  "json="+JSON.stringify(data) , on_success, true);
+    for(var i in ldc.OPERATIONS) {
+        if (ldc.OPERATIONS[i].id == id) {
+            delete(ldc.OPERATIONS[i]);
             break;
         }
     }
@@ -181,15 +333,15 @@ function ldc_op_del(id) {
 function ldc_op_update(op) {
     function on_success(data, textStatus) {
         if (data == 1) {
-            ldc_view_log_error("La modification de l'opération a échoué...");
+            ldc.view.log.error("La modification de l'opération a échoué...");
         } else {
-            ldc_view_log_success("Opération Modifiée.");
+            ldc.view.log.success("Opération Modifiée.");
         }
     }
-    ldc_post_ajax(LDC_SERVER + "update_operation.php",  "json="+JSON.stringify(op) , on_success, true);
-    for(var i in LDC_OPERATIONS) {
-        if (LDC_OPERATIONS[i].id == op.id) {
-            LDC_OPERATIONS[i] = op;
+    ldc.post_ajax(LDC_SERVER + "update_operation.php",  "json="+JSON.stringify(op) , on_success, true);
+    for(var i in ldc.OPERATIONS) {
+        if (ldc.OPERATIONS[i].id == op.id) {
+            ldc.OPERATIONS[i] = op;
             break;
         }
     }
